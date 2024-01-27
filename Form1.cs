e23893bdd3c0aa153b9c642ca072dd8bbebb3f98
Laughingninza <157867803+Laughingninza@users.noreply.github.com>
2024-01-27 10:25:08 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+             ProcessStartInfo startInfo = new ProcessStartInfo();
+             startInfo.FileName = "AcroRd32.exe";
+            string args = string.Format("-print-to-default \"{0}\" -exit-when-done", "xx.pdf");
+            startInfo.Arguments = args;
+            startInfo.CreateNoWindow = true;
+            startInfo.ErrorDialog = false;
+            startInfo.UseShellExecute = false;
+            Process process = Process.Start(startInfo); 
+            Console.WriteLine("Start Print.");
+        //process.Exited += process_Exited;
+        Recheck:
+            if (!process.HasExited)
+            {
+                Console.WriteLine("Wait 1 Sec.");
+                process.WaitForExit(1000);
+                goto Recheck;
+            }
+            else
+            {
+                Console.WriteLine("Print Finish.");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+
+        }
+           
+        }
+    }
+
